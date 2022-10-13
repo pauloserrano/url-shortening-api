c040dfb181d5ab21d7e16842ed3cbdba5bfcd46e
Paulo Serrano <pvsserrano@gmail.com>
2022-10-13 19:48:11 -0300

    url schema update

--- src/schemas/urls.schemas.js
+++ src/schemas/urls.schemas.js
@@ -2,7 +2,9 @@ import joi from "joi"
 
 
 const urlSchema = joi.object({
-    url: joi.string().required()
+    url: joi.string()
+        .pattern(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/)
+        .required()
 })
 
 
